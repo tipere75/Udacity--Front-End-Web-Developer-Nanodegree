0774a1077d2fce9e86d844be00907b0df75110a9
Pierre Hénin <pierre_henin@hotmail.fr>
2021-08-18 18:05:06 +0200

    Edit the navigation menu with javascript
    The navigation menu is flexible enough to accept new sections without
    revising all the css files

--- 2- Landing Page/js/platform.js
+++ 2- Landing Page/js/platform.js
@@ -1,7 +1,7 @@
 /*
 Define Global Variables
 */
-const platformNavigationMenu = document.querySelector('.platform-navbar-list');
+const platformNavigationMenu = document.querySelector('#platform-navbar-list');
 const platformSections = document.querySelectorAll('.platform-section');
 
 
@@ -23,13 +23,16 @@ function platformNavigationBarCreation () {
     const li_platformNavigationMenu = document.createElement('li');  // list element to display in the menu
     const a_platformNavigationMenu = document.createElement('a');    // anchor to go to the section
 
-    a_platformNavigationMenu.innerText = platformSection.getAttribute('data-nav');
+    a_platformNavigationMenu.innerText = platformSection.getAttribute('data-nav');  // get section name
+    a_platformNavigationMenu.setAttribute('class', 'platform-navbar-link');   // set class attribute
 
-
-    // add to the navigation menu
-
+    // add anchor to the list element then to the navigation bar
+    li_platformNavigationMenu.appendChild(a_platformNavigationMenu);
+    platformNavigationBar.appendChild(li_platformNavigationMenu);
   });
 
+  // add navigation bar to navigation menu
+  platformNavigationMenu.appendChild(platformNavigationBar);
 }
 
 // Add class 'active' to section when near top of viewport
