10a9f771fa55bcfbf079e8deae66455a5804718b
Pierre Hénin <pierre_henin@hotmail.fr>
2021-08-18 17:38:35 +0200

    Set up the header with raw inputs
    The navigation bar is set up in the index.html file rather than with
    javascript

--- 2- Landing Page/js/platform.js
+++ 2- Landing Page/js/platform.js
@@ -1,40 +1,36 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
+/*
+Define Global Variables
 */
-
-/**
- * Define Global Variables
- *
-*/
+const platformNavigationMenu = document.querySelector('.platform-navbar-list');
+const platformSections = document.querySelectorAll('.platform-section');
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
+/*
+Helper Functions
 */
 
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
+/*
+Main Functions
 */
 
 // build the nav
+function platformNavigationBarCreation () {
+  const platformNavigationBar = document.createDocumentFragment();
 
+  platformSections.forEach((platformSection, i) => {
+    const li_platformNavigationMenu = document.createElement('li');  // list element to display in the menu
+    const a_platformNavigationMenu = document.createElement('a');    // anchor to go to the section
+
+    a_platformNavigationMenu.innerText = platformSection.getAttribute('data-nav');
+
+
+    // add to the navigation menu
+
+  });
+
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -42,13 +38,12 @@
 // Scroll to anchor ID using scrollTO event
 
 
-/**
- * End Main Functions
- * Begin Events
- *
+/*
+Events
 */
 
 // Build menu
+platformNavigationBarCreation();
 
 // Scroll to section on link click
 
