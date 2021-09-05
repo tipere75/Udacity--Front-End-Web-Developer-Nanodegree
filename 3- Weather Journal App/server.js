02b12835f99e8dfe12a7e4ba0cbc2bc6829ffd6f
Pierre Hénin <pierre_henin@hotmail.fr>
2021-09-05 13:26:28 +0200

    Connect to openweather, set up requests and update ui

--- 3- Weather Journal App/server.js
+++ 3- Weather Journal App/server.js
@@ -1,26 +1,54 @@
+/*
+Setup environment
+*/
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
-// Express to run server and routes
-const express = require('express');
+// express to run server and routes
+const express = require('express')
 
-// Start up an instance of app
+// start up an instance of app
 const app = express();
 
-/* Dependencies */
+/* sependencies */
 const bodyParser = require('body-parser')
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+/* middleware*/
+// configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Cors for cross origin allowance
+// cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
 
-// set up folder
+// initialize main project folder
 app.use(express.static('website'));
 
-// Setup Server
+/*
+Requests
+*/
+// post Route
+app.post('/add', (req, res) => {
+    // update projectData
+    projectData['date'] = req.body.date;
+    projectData['temp'] = req.body.temp;
+    projectData['content'] = req.body.content;
+    console.log(JSON.stringify(projectData));
+    // send back projectData
+    res.send(projectData);
+});
+
+
+// initialize all route with a callback function
+app.get('/all', (req, res) => {
+    res.send(projectData);
+});
+
+
+/*
+Spin up server
+*/
 const port = 8080;
 // callback to debug
-const server = app.listen(port, () => {console.log(`running on localhost: ${port}`);});
+const server = app.listen(port, () => {
+    console.log(`running on localhost: ${port}`);
+});
--- 3- Weather Journal App/website/app.js
+++ 3- Weather Journal App/website/app.js
@@ -1,5 +1,104 @@
-/* Global Variables */
+/*
+Global variables
+*/
+// accessing openweathermap API
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=3ea004b560aa273fd59c1a502b4c82b2';
+const country = 'us';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+/*
+Function to get data from openweathermap api
+*/
+const getWeatherData = async (baseURL, zipCode, country, apiKey) => {
+    // ask for weather data from openweathermap api
+    const response = await fetch(baseURL + zipCode + `,` + country + apiKey);
+
+    try {
+        // return weather data if available
+        const newData = await response.json();
+        return newData;
+
+    } catch (error) {
+        // erorr handling
+        console.log("error", error);
+
+    }
+}
+
+/*
+Function to post data
+*/
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // Body data type must match "Content-Type" header
+        body: JSON.stringify(data)
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+
+      } catch(error) {
+          console.log("error", error);
+
+      }
+  }
+
+/*
+Function to update UI
+*/
+const updateUI = async () => {
+    const request = await fetch('/all');
+
+    try {
+        const allData = await request.json();
+        // display weather informations
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('temp').innerHTML = allData.temp;
+        document.getElementById('content').innerHTML = allData.content;
+
+        // reset inputs
+        document.getElementById('zip').value = '';
+        document.getElementById('feelings').value = '';
+
+    } catch (error) {
+        console.log('error', error);
+
+    }
+}
+
+/*
+Get and post weather data then update UI
+*/
+function displayWeatherData (event) {
+    // retrieve user inputs
+    const userZipCode = document.getElementById('zip').value;
+    const userFeelings = document.getElementById('feelings').value;
+
+    // chain get weather data + post data + update UI
+    getWeatherData(baseURL, userZipCode, country, apiKey)
+    .then((data) => {
+        postData('/add', {date: newDate, temp: data.main.temp, content: userFeelings});
+    })
+    .then (() => {
+        updateUI()
+    })
+    .catch(function(error) {
+        console.log(error);
+        alert('Invalid inputs format');
+    })
+}
+
+/*
+Event listener
+*/
+document.getElementById('generate').addEventListener('click', displayWeatherData);
