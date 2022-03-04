2d25bcf6fcd2861b0a619d2cce071a9fc355cb12
Hénin <pierre_henin@hotmail.fr>
2022-03-04 19:06:36 +0100

    [4-Evaluate news nlp] Update url checker function to stick with http protocol

--- 4- Evaluate news nlp/src/client/js/urlChecker.js
+++ 4- Evaluate news nlp/src/client/js/urlChecker.js
@@ -1,7 +1,7 @@
 function checkForURL(inputText) {
     console.log("::: Running checkForURL :::", inputText);
 
-    let urlRegex = new RegExp('^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?');
+    let urlRegex = new RegExp('^(http[s]?:\\/\\/(www\\.)){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?');
 
     return inputText.match(urlRegex) == null ? false:true;
 }
