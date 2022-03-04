4251e95533a59fa754edfbef54f877e3456b1570
Hénin <pierre_henin@hotmail.fr>
2022-03-04 18:29:45 +0100

    [4-Evaluate news nlp] Fix display

--- 4- Evaluate news nlp/src/client/index.js
+++ 4- Evaluate news nlp/src/client/index.js
@@ -1,5 +1,11 @@
-// import { checkForName } from './js/nameChecker'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+
 import { handleSubmit } from './js/formHandler'
 import { polarityMapping } from './js/formHandler'
+import { checkForURL } from './js/urlChecker'
 
-export { handleSubmit, polarityMapping }
+export { handleSubmit, polarityMapping, checkForURL }
