78a08ed869f3d1ec8a52484cb7ea373114d69ec0
Hénin <pierre_henin@hotmail.fr>
2022-03-04 13:47:57 +0100

    [4-Evaluate news nlp]Configure webpack.prod.js

--- 4- Evaluate news nlp/webpack.prod.js
+++ 4- Evaluate news nlp/webpack.prod.js
@@ -1,23 +1,38 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
+  entry: './src/client/index.js',
+  mode: 'production',
+  output: {
+    libraryTarget: 'var',
+    library: 'Client'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new WorkboxPlugin.GenerateSW(),
+    new MiniCssExtractPlugin({ filename: "[name].css" })
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({}),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
 }
