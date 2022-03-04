8c4ced67749d2aa5b936e2b3808c628f58453816
Hénin <pierre_henin@hotmail.fr>
2022-03-04 14:28:43 +0100

    [4-Evaluate news nlp]Set up prod and dev configuration; rename server as server.js

--- 4- Evaluate news nlp/webpack.prod.js
+++ 4- Evaluate news nlp/webpack.prod.js
@@ -2,9 +2,9 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
-const WorkboxPlugin = require('workbox-webpack-plugin')
+// const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   entry: './src/client/index.js',
@@ -26,7 +26,7 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
-    new WorkboxPlugin.GenerateSW(),
+    // new WorkboxPlugin.GenerateSW(),
     new MiniCssExtractPlugin({ filename: "[name].css" })
   ],
   optimization: {
