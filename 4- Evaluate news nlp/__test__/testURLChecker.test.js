d4c3232cd61af940a4ecf54be892090950973d23
Hénin <pierre_henin@hotmail.fr>
2022-03-04 19:07:25 +0100

    [4-Evaluate news nlp] Tests for url checker

---
+++ 4- Evaluate news nlp/__test__/testURLChecker.test.js
@@ -0,0 +1,20 @@
+import { checkForURL } from "../src/client/js/urlChecker"
+
+describe("Testing the checkForUrl() function", () => {
+    test("Testing for missing https/http", () => {
+        let url = 'www.testhttp.com'
+        expect(checkForURL(url)).toBe(false);
+    });
+    test("Testing for missing www", () => {
+        let url = 'http://testwww.com'
+        expect(checkForURL(url)).toBe(false);
+    });
+    test("Testing for space", () => {
+        let url = 'https://www.test blank space.com'
+        expect(checkForURL(url)).toBe(false);
+    });
+    test("Testing for valid url", () => {
+        let url = 'https://www.validurl.com'
+        expect(checkForURL(url)).toBe(true);
+    });
+});
