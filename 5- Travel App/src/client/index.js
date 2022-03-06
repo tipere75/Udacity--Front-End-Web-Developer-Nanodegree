b4426a211e14e81151279ea1c5fba4f16ca17a76
Hénin <pierre_henin@hotmail.fr>
2022-03-06 19:35:18 +0100

    [5] Initial commit with webpack initial configuration

---
+++ 5- Travel App/src/client/index.js
@@ -0,0 +1,9 @@
+import './styles/firstSCSSFile.scss'
+
+import { functionName1 } from './js/fileName1'
+import { functionName2 } from './js/fileName2'
+
+export {
+    functionName1,
+    functionName2
+}
---
+++ 5- Travel App/webpack.prod.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    output: {
+         libraryTarget: 'var',
+         library: 'Client'
+     },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new WorkboxPlugin.GenerateSW(),
+        new MiniCssExtractPlugin({ filename: "[name].css" })
+    ],
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    }
+}
