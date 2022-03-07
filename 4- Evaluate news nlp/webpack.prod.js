d46d6e535268357db82ba6a289f24a4e9c7f5093
Hénin <pierre_henin@hotmail.fr>
2022-03-07 21:32:43 +0100

    [4] Activate Service Worker in prod environment

--- 4- Evaluate news nlp/webpack.prod.js
+++ 4- Evaluate news nlp/webpack.prod.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
-// const WorkboxPlugin = require('workbox-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   entry: './src/client/index.js',
@@ -26,7 +26,7 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
-    // new WorkboxPlugin.GenerateSW(),
+    new WorkboxPlugin.GenerateSW(),
     new MiniCssExtractPlugin({ filename: "[name].css" })
   ],
   optimization: {
